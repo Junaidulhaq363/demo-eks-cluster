528fcf8b49125df74dc9ed9c60f1f4d044b6928e
junaidul haq <junaidul.haq@tothenew.com>
2023-03-02 22:00:58 +0530

    add

--- main.tf
+++ main.tf
@@ -141,25 +141,25 @@ resource "aws_security_group_rule" "this" {
   )
 }
 
-resource "aws_kms_key" "example_key" {
-  description = "Example KMS key"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-        Action = [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+# resource "aws_kms_key" "example_key" {
+#   description = "Example KMS key"
+#   policy      = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect   = "Allow"
+#         Principal = {
+#           AWS = "*"
+#         }
+#         Action = [
+#           "kms:Encrypt",
+#           "kms:Decrypt",
+#           "kms:ReEncrypt*",
+#           "kms:GenerateDataKey*",
+#           "kms:DescribeKey"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
